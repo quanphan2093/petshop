c754984116fe90fe968a7134d4f2a8766d288c81
quanphan2k3 <quanpahe170260@fpt.edu.vn>
2024-12-30 23:45:51 +0700

    register feature
    fix: fix front-end for customer page
    
    register

---
+++ PetStore/Pages/Common/Mail.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace PetStore.Pages.Common
+{
+	public class Mail
+	{
+		public static Mail instance = new Mail();
+		public Mail()
+		{
+			if (instance == null) instance = this;
+		}
+		public void sendMail(string from, string password, string to, string body, string subject)
+		{
+			string fromMail = from;
+			string fromPass = password;
+			MailMessage message = new MailMessage();
+			message.From = new MailAddress(fromMail);
+			message.Subject = subject;
+			message.To.Add(new MailAddress(to));
+			message.Body = body;
+			message.IsBodyHtml = true;
+			var smtpClient = new SmtpClient("smtp.gmail.com")
+			{
+				Port = 587,
+				Credentials = new NetworkCredential(fromMail, fromPass),
+				EnableSsl = true
+			};
+			smtpClient.Send(message);
+		}
+	}
+}
