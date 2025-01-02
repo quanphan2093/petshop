7c6cffbb8d89f4ec685d57dc3e18b144d6c01b25
quanphan2k3 <quanpahe170260@fpt.edu.vn>
2025-01-02 22:45:14 +0700

    homepage

--- PetStore/obj/Debug/net6.0/PetStore.AssemblyInfo.cs
+++ PetStore/obj/Debug/net6.0/PetStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9147c92346e18eb2ccee28f00695087ed416aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
