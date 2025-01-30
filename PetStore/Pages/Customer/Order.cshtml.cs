8ae22c6a50a49478e553acb8cd58cf4a0d35c0c1
quanphan2k3 <quanpahe170260@fpt.edu.vn>
2025-01-30 20:51:49 +0700

    view order

--- PetStore/Pages/Customer/Order.cshtml.cs
+++ PetStore/Pages/Customer/Order.cshtml.cs
@@ -14,17 +14,18 @@ namespace PetStore.Pages.Customer
         public List<StatusOrder> statusOrders = new List<StatusOrder>();
         public PetStoreContext _context;
         public string s;
+        public string Search;
         public OrderModel(PetStoreContext context)
         {
             _context=context;
         }
-        public IActionResult OnGet(string status)
+        public IActionResult OnGet(string status, string search)
         {
-            int? accId = HttpContext.Session.GetInt32("acc");
-            if (accId == null)
-            {
-                return RedirectToPage("/Common/Login");
-            }
+            //int? accId = HttpContext.Session.GetInt32("acc");
+            //if (accId == null)
+            //{
+            //    return RedirectToPage("/Common/Login");
+            //}
             products =_context.Products.ToList();
             categories=_context.Categories.ToList();
             ordersDetail=_context.OrderDetails.ToList();
@@ -35,7 +36,7 @@ namespace PetStore.Pages.Customer
                         join p in products on od.ProductId equals p.ProductId
                         join c in categories on p.CategoryId equals c.CategoryId
                         join s in statusOrders on o.StatusId equals s.StatusId
-                        where o.AccountId == accId
+                        //where o.AccountId == accId
                         select new
                         {
                             OrderId = o.OrderId,
@@ -52,8 +53,13 @@ namespace PetStore.Pages.Customer
             {
                 order = order.Where(x => x.Status.Equals(status, StringComparison.OrdinalIgnoreCase));
             }
+            if(!string.IsNullOrEmpty(search))
+            {
+                order= order.Where(x => x.ProductName.Contains(search, StringComparison.OrdinalIgnoreCase));
+            }
             order = order.OrderByDescending(x => x.CreateAt);
             s = status;
+            Search = search;
             ViewData["order"]= order.ToList();
             return Page();
         }
