270e047e4087b93761224313fc889a331c4f4e0c
quanphan2k3 <quanpahe170260@fpt.edu.vn>
2025-02-16 23:40:19 +0700

    fix

--- PetStore/Program.cs
+++ PetStore/Program.cs
@@ -20,9 +20,12 @@ builder.Services.AddSession(options =>
 });
 var app = builder.Build();
 
-app.UseExceptionHandler("/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
+// Handle errors properly in Production
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
