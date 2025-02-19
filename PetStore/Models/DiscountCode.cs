b774b9c78d421b677e7027ba85966aafb02701a6
Duy Hai <145411748+haitd2810@users.noreply.github.com>
2025-02-19 21:00:13 +0700

    feat: impl discount

---
+++ PetStore/Models/DiscountCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetStore.Models
+{
+    public partial class DiscountCode
+    {
+        public DiscountCode()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int CodeId { get; set; }
+        public string? Code { get; set; }
+        public int? DiscountPercent { get; set; }
+        public string? Status { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
